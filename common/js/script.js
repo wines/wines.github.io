ca3dedbd62e0bd3c76fab6bb4a542f08ee8e2ea5
takumaro-web <ta7ku2ma3@gmail.com>
2015-03-19 16:31:59 +0900

    修正

--- common/js/script.js
+++ common/js/script.js
@@ -9,12 +9,17 @@ $(function() {
   var demoTimeout;
   $('#num1, #num2, #num3, #num4, #num5, #num6, #num7, #num8, #num9, #num10, #num11, #num12, #num13, #num14, #num15, #num16, #num17, #num18, #num19, #num20, #num21, #num22, #num23, #num24, #num25, #num26, #num27, #num28, #num29, #num30, #num31, #num32, #num33, #num34, #num35, #num36, #num37, #num38, #num39, #num40').bind({
     'touchstart mousedown': function(e) {
-    e.preventDefault();
+    //e.preventDefault();
     $this = $(this);
     clearTimeout(demoTimeout);
     $this.trigger('startRumble');
     demoTimeout = setTimeout(function(){$this.trigger('stopRumble');}, 1500);
   }});
+
+  $('#num15').bind({
+    'touchstart mousedown': function(e) {
+      alert("当たりです!!!!おめでとうございます!!!!!さすがですね!!!!!良く発見できましたね。。すごいです。信じられないです。これから運気バンバン上がりますよ!!!きっと。");
+    }});
 
 });
 
@@ -31,7 +36,6 @@ if(window.addEventListener) {
 
 /* シェアボタンを読み込む関数 */
 function shareButtonReadSyncer(){
-setTimeout(function(){
 //Twitter
 window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));
 
@@ -60,9 +64,6 @@ document.getElementsByTagName("head")[0].appendChild(scriptTag);
 
 //pocket
 (!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pocket-btn-js"));
-
-},5000);  //ページを開いて5秒後(5,000ミリ秒後)にシェアボタンを読み込む
-
 }
 
 /*-------------------------------------------
