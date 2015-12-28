d48c9eb215b3574a0511febecee538bc9cebaa6f
takumaro-web <ta7ku2ma3@gmail.com>
2015-12-29 02:10:47 +0900

    修正

--- soutsu/common/js/game.js
+++ soutsu/common/js/game.js
@@ -9,6 +9,7 @@ var ROLL_NUMVER = 0;
 var GAME_FLAG = false;
 var START_FLAG = false;
 var levelTxt,levelTxten;
+var $url = $("meta[property='og:url']").attr("content");
 
 var $gameretry = $('#js-retry');
 var $gameover = $('#js-gameover');
@@ -59,6 +60,7 @@ function countdown($call, $count) {
       $("#level").text(levelTxt);
       $gameover.addClass('block');
       $gameretry.addClass('block');
+      $("#tweet").append('<a href="http://twitter.com/intent/tweet?original_referer='+$url+'&amp;url='+$url+'&amp;text=%e2%96%a0%e8%ba%81%e9%ac%b1%e3%83%ac%e3%83%99%e3%83%ab%ef%bc%9a'+levelTxten+'%e2%96%a0%e9%80%a3%e6%89%93%e5%9b%9e%e6%95%b0%ef%bc%9a'+ROLL_NUMVER+'%e5%9b%9e%e3%81%93%e3%82%93%e3%81%aa%e3%81%93%e3%81%a8%e3%81%a3%e3%81%a6%e3%81%82%e3%82%8a%e3%81%88%e3%81%be%e3%81%99%ef%bc%9f%23%e8%ba%81%e9%ac%b1%e3%81%8f%e3%82%93%e3%81%ae%e3%82%b2%e3%83%bc%e3%83%a0%23%e8%ba%81%e9%ac%b1%e6%bc%ab%e7%94%bb%e9%9b%86" target="_blank"><i class="fa fa-twitter"></i> 結果をツイート</a>');
     }
 }
 
@@ -92,7 +94,7 @@ var startFunc = function(start){
 };
 
 //sp
-$IMG.on('touchstart touchmove touchend',function(event){
+$gameretry.on('touchstart touchmove touchend',function(event){
   if ('touchstart' == event.type){
     $(this).attr('data-touchstarted', '');
     return;
@@ -102,18 +104,19 @@ $IMG.on('touchstart touchmove touchend',function(event){
     return;
   }
   if ('undefined' != typeof $(this).attr('data-touchstarted')){
-    gemaRetry(($this));
+    gameRetry(($this));
     $(this).removeAttr('data-touchstarted');
     return false;
   }
 });
 //pc
 $gameretry.on("click",function(){
-  gemaRetry(($this));
+  gameRetry($(this));
   return false;
 });
 
-var gemaRetry = function(retry) {
+var gameRetry = function(retry) {
+  $("#tweet").find("a").remove();
   retry.removeClass('block');
   $gameover.removeClass('block');
   $("#btn-area").addClass('block');
@@ -128,31 +131,32 @@ $('#js-btn').on("click touchstart",function(){
   if(GAME_FLAG === false && START_FLAG === true){
     ROLL_NUMVER++;
     $("#rollnumver").text(ROLL_NUMVER);
-    if( ROLL_NUMVER <= 10 ){
-      levelTxt = "カス";
+    if( 0 < ROLL_NUMVER && ROLL_NUMVER <= 10 ){
+      levelTxt = "かす";
       levelTxten = "%e3%82%ab%e3%82%b9";
     }else if( 10 < ROLL_NUMVER && ROLL_NUMVER <= 20 ){
-      levelTxt = "耳くそ";
-      levelTxten = "%e8%80%b3%e3%81%8f%e3%81%9d";
+      levelTxt = "ぃぼぢ";
+      levelTxten = encodeURI(levelTxt);
     }else if( 20 < ROLL_NUMVER && ROLL_NUMVER <= 40 ){
       levelTxt = "ぅンこ";
-      levelTxten = "%e3%81%85%e3%83%b3%e3%81%93";
-    }else if( 40 < ROLL_NUMVER && ROLL_NUMVER <= 100 ){
-      levelTxt = "ふッぅ";
-      levelTxten = "%e3%81%b5%e3%83%83%e3%81%85";
+      levelTxten = encodeURI(levelTxt);
+    }else if( 40 < ROLL_NUMVER && ROLL_NUMVER <= 80 ){
+      levelTxt = "ふっぅ";
+      levelTxten = encodeURI(levelTxt);
+    }else if( 80 < ROLL_NUMVER && ROLL_NUMVER <= 120 ){
+      levelTxt = "ゎたみ";
+      levelTxten = encodeURI(levelTxt);
     }else{
       levelTxt = "ぉっゅ";
-      levelTxten = "%e3%81%89%e3%81%a3%e3%82%85";
+      levelTxten = encodeURI(levelTxt);
     }
   }
 });
 
-$("#tweet a").on("click touchstart",function(){
+$(document).on("click touchstart","#tweet a",function(){
   if(ROLL_NUMVER === 0) {
-    alert("記録ねェーぞ。ちゃンとャれ。");
+    alert("きろくねぇ~ぞ、ちゃンとゃれこのゃろ~~~~ なンで…ちょ~せンしてもねぇ~のに、っぃ~としょ~と、してゃがンだこのゃろ~~…ぉまぇのぉくさンの…ぃろンな…ぃろンな…………ぃろンな…むだにすンじゃね~~ぞこら~~~~~ そンなンだから……ぉくさンのぉっゅが…かゎくンだろが~~~ とにも…かくにも…そンなんじゃ…ぁかンのょ  けンこンぃッてき…ちょ~せンしろこら~~~~~~");
     return false;
-  }else{
-    $(this).attr('href','http://twitter.com/share?url=http%3A%2F%2Fwines.github.io%2Fsoutsu%2Fgame.html&amp;text=%e2%96%a0%e8%ba%81%e9%ac%b1%e3%83%ac%e3%83%99%e3%83%ab%ef%bc%9a'+levelTxten+'%e2%96%a0%e9%80%a3%e6%89%93%e5%9b%9e%e6%95%b0%ef%bc%9a'+ROLL_NUMVER+'%e5%9b%9e%e3%80%82\n%e3%81%93%e3%82%93%e3%81%aa%e3%81%93%e3%81%a8%e3%81%a3%e3%81%a6%e3%81%82%e3%82%8a%e3%81%88%e3%81%be%e3%81%99%ef%bc%9f\n%23%e8%ba%81%e9%ac%b1%e3%81%8f%e3%82%93%e3%81%ae%e3%82%b2%e3%83%bc%e3%83%a0%23%e8%ba%81%e9%ac%b1%e6%bc%ab%e7%94%bb%e9%9b%86');
   }
 });
 
